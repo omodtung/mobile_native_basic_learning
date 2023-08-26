4f244f02b38dc19a11bea42d450df000da3e49d2
omodtung <dothedung131@gmail.com>
2023-08-26 22:58:40 +0700

    activity life circle

---
+++ MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.lifeofactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("//------------", "------------- onCreate");
+    }
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.i("//------------", "------------- onStart ");
+    }
+
+    @Override
+    protected  void onResume()
+    {
+        super.onResume();
+        Log.i("//------------", "------------- onResume");
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.i("//------------", "-------------  onPause");
+    }
+
+
+    @Override
+    protected  void onStop()
+    {super.onStop();
+        Log.i("//------------", "------------- onStop ");
+
+    }
+    @Override
+    protected  void  onDestroy()
+    {
+        super.onDestroy();
+        Log.i("//------------", "------------- onDestroy ");
+
+    }
+
+
+    @Override
+    protected  void onRestart()
+    {
+    super.onRestart();
+        Log.i("//-----------", "-------------  onRestart");
+
+    }
+
+
+}
